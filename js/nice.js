53204c5b898a60a229bb61485d55e8093f94f5d7
淼先森 <38773605+vainmusic@users.noreply.github.com>
2021-06-13 17:08:56 +0800

    8键

--- js/nice.js
+++ js/nice.js
@@ -2,7 +2,40 @@
 	document.body.addEventListener('touchmove', function (e) {
                 e.preventDefault()
             }, { passive: false });
-    
+
+function touchDwon(evt) {
+    nes.keyboard.keyDown({
+        keyCode: evt.keyCode
+    });
+};
+
+function touchUp(evt) {
+    nes.keyboard.keyUp({
+        keyCode: evt.keyCode
+    });
+};
+
+function select(val) {
+    if (val.substring(0, 1) == "#") {
+        return document.querySelector(val);
+    } else {
+        return document.getElementById(val);
+    }
+}
+
+var joystick = new Joystick({
+    el: select("controls-rocker"),
+    mode: 'static',
+    color: "#808080",
+    size: 150,
+    isFourBtn: false,
+    keyCodes: [87, 83, 65, 68],
+    btn_down_fn: touchDwon,
+    btn_up_fn: touchUp,
+    relative: true,
+});
+joystick.init();
+
 var nes;
 	    $(function() {
 	        nes = new JSNES({
